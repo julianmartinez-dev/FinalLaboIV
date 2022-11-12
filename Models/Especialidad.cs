14381894495309c8475366081aa9c20cb5103d52
Julian Martinez <julian.fmartinez@gmail.com>
2022-11-11 23:06:21 -0300

    data-annotations para vista de medicos

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -2,10 +2,13 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Turnos.Models
-{
+{   
     public class Especialidad {
         [Key]
         public int IdEspecialidad { get; set; }
+        [StringLength(50, ErrorMessage = "El nombre de la especialidad no puede superar los 50 caracteres")]
+        [Required (ErrorMessage = "Debe ingresar una descripcion")]
+        [Display(Name = "Descripción", Prompt = "Ingrese una Descripción")]
         public string Descripcion { get; set; }
         public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
     }
--- Models/Medico.cs
+++ Models/Medico.cs
@@ -8,14 +8,30 @@ namespace Turnos.Models
     {
         [Key]
         public int IdMedico { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un apellido")] 
         public string Apellido { get; set; }
+        [Required (ErrorMessage = "Debe ingresar un matricula")]
         public string Matricula { get; set; }
+        [Display(Name = "Teléfono", Prompt = "Ingrese un Teléfono")]
+        [Required (ErrorMessage = "Debe ingresar un teléfono")]
         public string Telefono { get; set; }
+        [Required (ErrorMessage = "Debe ingresar un email")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un email válido")]
         public string Email { get; set; }
+        [Display(Name = "Dirección", Prompt = "Ingrese una Dirección")]
+        [Required (ErrorMessage = "Debe ingresar una dirección")]
         public string Direccion { get; set; }
+        [Required(ErrorMessage = "Debe ingresar una localidad")]
         public string Localidad { get; set; }
+        [Display(Name = "Horario de entrada")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime HorarioEntrada { get; set; }
+        [Display(Name = "Horario de salida")]
+        [DataType(DataType.Time)]
+        [DisplayFormat (DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime HorarioSalida { get; set; }
         public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
     }
